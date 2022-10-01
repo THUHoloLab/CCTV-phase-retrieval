270dd476222d2835897a38cf797f9f21bd6e3e46
Yunhui-Gao <gaoyh99@outlook.com>
2022-10-01 13:40:28 +0800

    modify the propagation function

--- main/utils/propagate.m
+++ main/utils/propagate.m
@@ -1,4 +1,15 @@
-                                                                                                                                               function [ w_o ] = propagate( w_i, dist, pxsize, wavlen, method )
+function [ w_o ] = propagate( w_i, dist, pxsize, wavlen, method )
+% -----------------------------------------------------------------
+% This function numerically simulates the free-space propagation of a 
+% complex wavefield.
+% -----------------------------------------------------------------
+%   INPUT   w_i    : Input complex wavefield
+%           dist   : Propagation distance
+%           pxsize : Pixel size
+%           wavlen : Wavelength
+%           mtehod : Numerical method ('Fresnel' or 'Angular Spectrum')
+%   OUTPUT  w_o    : Output wavefield after propagation
+% -----------------------------------------------------------------                                                                                                                   function [ w_o ] = propagate( w_i, dist, pxsize, wavlen, method )
 
 [N,M] = size(w_i);    % size of the wavefront
 
@@ -13,7 +24,14 @@ inputFT = fftshift(fft2(w_i));
 if strcmp(method,'Fresnel')
     H = exp(1i*k*dist)*exp(-1i*dist*(KX.^2+KY.^2)/2/k);
 elseif strcmp(method,'Angular Spectrum')
-    H = exp(1i*dist*sqrt(k^2-KX.^2-KY.^2));
+    % remove evanescent orders
+    KX_m = KX;
+    KY_m = KY;
+    ind = (KX.^2+KY.^2 >= k^2);
+    KX_m(ind) = 0;
+    KY_m(ind) = 0;
+    % transfer function
+    H = exp(1i*dist*sqrt(k^2-KX_m.^2-KY_m.^2));
 else
     errordlg('Wrong parameter for [method]: must be <Angular Spectrum> or <Fresnel>','Error');
 end
